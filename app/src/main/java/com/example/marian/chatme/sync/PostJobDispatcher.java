8ebfa299058983af6d142d0cb4b013677fcba86a
ragaamaherhanna2 <ragaamaherhanna@gmail.com>
2017-08-08 19:58:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/marian/chatme/sync/PostJobDispatcher.java
@@ -0,0 +1,42 @@
+package com.example.marian.chatme.sync;
+
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class PostJobDispatcher extends JobService {
+    private AsyncTask mAsyncTask;
+
+    @Override
+    public boolean onStartJob(final JobParameters jobParameters) {
+        mAsyncTask = new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] params) {
+                Context context = PostJobDispatcher.this;
+                // MainActivity.doSomething(context,MainActivity.ACTION_REMINDER);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Object o) {
+
+                jobFinished(jobParameters, false);
+            }
+        };
+        mAsyncTask.execute();
+
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        if (mAsyncTask != null) mAsyncTask.cancel(true);
+        return true;
+    }
+}
